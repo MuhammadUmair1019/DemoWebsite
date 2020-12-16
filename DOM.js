1223df526ee29bb2bdc376d178019a5be078472c
Muhammad umair <62468878+MuhammadUmair1019@users.noreply.github.com>
2020-12-16 08:57:44 +0500

    Add files via upload

---
+++ DOM.js
@@ -0,0 +1,9 @@
+// var element;
+// element = document.getElementById("about").innerText;
+// element = document.getElementById("about").innerHTML;
+// element = document.getElementById("about").getAttribute("class");
+// element = document.getElementById("about").getAttributeNode("id");
+// element = document.getElementById("about").attributes;
+
+// console.log(element);
+
